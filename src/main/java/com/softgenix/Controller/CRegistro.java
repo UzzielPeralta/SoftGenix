3890690efc950538645f7a2369703f0a0337e1af
Mujica Aguilar Angel <20243ds049@utez.edu.mx>
2025-06-30 22:22:52 -0600

    Declaraciones de paquete, imports y clase

--- src/main/java/com/softgenix/Controller/CRegistro.java
+++ src/main/java/com/softgenix/Controller/CRegistro.java
@@ -1,4 +1,15 @@
 package com.softgenix.Controller;
 
+
+import com.softgenix.Application.App;
+import com.softgenix.Application.Database;
+import com.softgenix.Utils.Path;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class CRegistro {
 }
