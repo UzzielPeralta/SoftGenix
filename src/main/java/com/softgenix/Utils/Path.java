9e3344741b39f1277c3e6fea78915fb6b37abd51
erick <20243ds054@utez.edu.mx>
2025-07-08 18:55:37 -0600

    Pull request

--- src/main/java/com/softgenix/Utils/Path.java
+++ src/main/java/com/softgenix/Utils/Path.java
@@ -4,6 +4,4 @@ public class Path {
         public static final String Sesion = "/FXML/Sesion.fxml";
         public static final String Registrar = "/FXML/Registro.fxml";
         public static final String LoginCss = "/Styles/Login.css";
-        public static final String Main = "/FXML/Main.fxml";
-
 }
